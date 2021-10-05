ee9c0ede99715cbb16bc8a286582879743d82a5a
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-10-05 14:47:12 -0300

    Add headers in estressados/main.cpp

--- estressados/main.cpp
+++ estressados/main.cpp
@@ -3,6 +3,9 @@
 #include "./includes/busca.hpp"
 #include "./includes/melhorCaso.hpp"
 #include "./includes/contagem.hpp"
+#include "./includes/filter.hpp"
+#include "./includes/acesso.hpp"
+#include "./includes/conjuntos.hpp"
 
 int main() {
 
