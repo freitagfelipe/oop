780a7c4b0f3112bab9a887bffcdd483f383e78f9
Felipe Rodrigues de Santana Freitag <felipefreitag08@hotmail.com>
2021-10-09 19:39:13 -0300

    Update abandonados to fix find error

--- estressados/includes/conjuntos.hpp
+++ estressados/includes/conjuntos.hpp
@@ -26,15 +26,12 @@ std::vector<int> diferentes(const std::vector<int> &fila) {
 
 std::vector<int> abandonados(const std::vector<int> &fila) {
     std::vector<int> auxiliar;
-    std::vector<int>::iterator it = auxiliar.begin();
 
-    for (int i = 0; i < fila.size(); i++) {
-        if (std::find(fila.begin(), fila.end(), fila[i]) == auxiliar.end()) {
+    for (int i = 0; i < int(fila.size()); i++) {
+        if (std::find(auxiliar.begin(), auxiliar.end(), fila[i]) == auxiliar.end()) {
             int quantasVezesAparece = std::count(fila.begin(), fila.end(), fila[i]);
 
-            auxiliar.insert(it, quantasVezesAparece - 1, fila[i]);
-
-            it = auxiliar.begin() + auxiliar.size();
+            auxiliar.insert(auxiliar.end(), quantasVezesAparece - 1, fila[i]);
         }
     }
 
